a2c9b3a2a9bcee20a6a4f50b393cee0877bb1bc3
Martin Hoecker-Martinez <mhoecker@coas.oregonstate,edu>
2013-02-22 14:53:09 -0800

    epand the plot doamin to show zero modes

--- slabstats.m
+++ slabstats.m
@@ -31,9 +31,11 @@ dxnc = Lx/(Nxnc+1);
 Dx = xnc-xnc(ceil((Nxnc+1)/2));
 Dy = ync-ync(ceil((Nync+1)/2));
 k = (1:1:Nxnc)-ceil((Nxnc+1)/2);
-k = k*2*pi/Lx;
+dk = 2*pi/Lx;
+k = k*dk;
 l = (1:1:Nync)-ceil((Nync+1)/2);
-l = l*2*pi/Ly;
+dl = 2*pi/Ly;
+l = l*dl;
 m = (1:1:Nznc)-ceil((Nznc+1)/2);
 m = m*2*pi/Lz;
 #
@@ -72,6 +74,7 @@ for i=1:Ntnc
 	PTz(:,:,i) = (abs(FTz(:,:,i))^2)/(Nxnc*Nync);
 	RTz(:,:,i) = fftshift(real(ifft2(ifftshift(PTz(:,:,i)))));
 	RTz(:,:,i) = (RTz(:,:,i)/max(max(RTz(:,:,i)))).^2;
+	PTzmax = max(max(PTz(:,:,i)));
 #
 	DTdat = [tmp "SurfTempAnomoly" num ".dat"];
 	PTdat = [tmp "SurfTempPSD" num ".dat"];
@@ -90,26 +93,27 @@ for i=1:Ntnc
 	fprintf(fid,"set size ratio -1\n");
 	fprintf(fid,"set pm3d\n");
 	fprintf(fid,"set palette mode HSV\n");
-	fprintf(fid,"tri(x) = (x-floor(x))\n");
 	fprintf(fid,"set palette maxcolor 128 function .8*(1-gray),.5+.5*ceil(127./128-gray),.5+.5*floor(127./128+gray)\n");
 	fprintf(fid,"unset surface\n");
 	fprintf(fid,"unset key\n");
+	fprintf(fid,"set xtics out\n");
+	fprintf(fid,"set ytics out\n");
 #
 	fprintf(fid,"set title 'SST anomaly (C) at t = %gs'\n",tnc(i));
 	fprintf(fid,"set xrange [%f:%f]\n",min(xnc),max(xnc));
 	fprintf(fid,"set xlabel 'Crosswind Dist. (m)'\n");
 	fprintf(fid,"set yrange [%f:%f]\n",min(ync),max(ync));
 	fprintf(fid,"set ylabel 'Upwind Dist. (m)'\n");
-	fprintf(fid,"set cbrange [-%e:%e]\n",5*rmsTz(i),5*rmsTz(i));
+	fprintf(fid,"set cbrange [-%e:%e]\n",7*rmsTz(i),7*rmsTz(i));
 	fprintf(fid,"splot '%s' matrix binary notitle\n",DTdat);
 #
 	fprintf(fid,"set output '%s'\n",[output_dir "SurfTempStat_PSD" num termsfx]);	
 	fprintf(fid,"set title 'Spectral Density (C^2 m^2 / rad^2) at t = %g'\n",tnc(i));
 	fprintf(fid,"set xrange [%f:%f]\n",min(k)/3,max(k)/3);
-	fprintf(fid,"set yrange [%f:%f]\n",0,max(l)/2);
+	fprintf(fid,"set yrange [%f:%f]\n",-dl,max(l)/2);
 	fprintf(fid,"set xlabel 'Crosswind Wave # (rad/m)'\n");
 	fprintf(fid,"set ylabel 'Upwind Wave # (rad/m)'\n");
-	fprintf(fid,"set cbrange [%e:%e]\n",3*rmsTz(i)/sqrt(Nxnc*Nync),12*rmsTz(i));
+	fprintf(fid,"set cbrange [%e:%e]\n",PTzmax/sqrt(Nxnc*Nync),PTzmax);
 	fprintf(fid,"set logscale cb\n");
 	fprintf(fid,"set palette maxcolor 128 function .8*(1-gray),.5+.5*ceil(127./128-gray),.5+.5*floor(127./128+gray)\n");
 	fprintf(fid,"splot '%s' matrix binary notitle\n",PTdat);
@@ -120,9 +124,11 @@ for i=1:Ntnc
 	fprintf(fid,"set yrange [%f:%f]\n",0,max(Dy));
 	fprintf(fid,"set xlabel 'Crosswind Dist. (m)'\n");
 	fprintf(fid,"set ylabel 'Upwind Dist. (m)'\n");
-	fprintf(fid,"set cbrange [%f:1]\n",6/(Nxnc*Nync));
+	fprintf(fid,"set cbrange [1e-3:1]\n");
 	fprintf(fid,"splot '%s' matrix binary notitle\n",RTdat);
 #
+	fprintf(fid,"set xtics in\n");
+	fprintf(fid,"set ytics in\n");
 	fprintf(fid,"unset title\n");
 	fprintf(fid,"unset colorbox\n");
 	fprintf(fid,"set output '%s'\n",[output_dir "SurfTempStat_R1D" num termsfx]);
@@ -131,13 +137,11 @@ for i=1:Ntnc
 	fprintf(fid,"set size ratio 0\n");
 	fprintf(fid,"set key\n");
 	fprintf(fid,"set xrange [%f:%f]\n",0,max(Dx));
-	fprintf(fid,"set yrange [%f:1]\n",6/(Nxnc*Nync));
+	fprintf(fid,"set yrange [1e-3:1]\n");
 	fprintf(fid,"set ylabel 'Autocorrelation^2'\n");
 	fprintf(fid,"set xlabel 'Crosswind Dist. (m)'\n");
 	fprintf(fid,"plot '%s' matrix binary u 1:3:3 every :::192::192 w linespoints title 'Crosswind' lw 2 pt 1 ps 2\n",RTdat);
 	fprintf(fid,"set xrange [%f:%f]\n",0,max(Dy));
-	fprintf(fid,"set yrange [%f:1]\n",6/(Nxnc*Nync));
-	fprintf(fid,"set ylabel 'Autocorrelation^2'\n");
 	fprintf(fid,"set xlabel 'Upwind Dist. (m)'\n");
 	fprintf(fid,"plot '%s' matrix binary u 1:3:3 every :::192::192 w linespoints title 'Upwind' lt 2 lw 2 pt 8 ps 2\n",RTtdat);
 #
