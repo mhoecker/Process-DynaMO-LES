030f12a28a9fffad980fee63500f1d5889a42de0
Martin Hoecker-Martinez <mhoecker@coas.oregonstate,edu>
2013-02-22 14:16:16 -0800

    add pngposter format
    make a LARGE png suitible for printing on a poster

--- termselect.m
+++ termselect.m
@@ -16,6 +16,10 @@ endif
 		case 'epsposter'
 			termtxt = 'postscript eps enhanced color size 12in,9in font "Helvetica,48" blacktext linewidth 2';
 			termsfx = '.eps';
+		case 'pngposter'
+			# if blown up to 12inx9in creates a 300dpi image
+			termtxt = 'png enhanced butt truecolor size 3600,2700 crop font "Helvetica,64" linewidth 6';
+			termsfx = '.png';
 		case 'epsarticle'
 			termtxt = 'postscript eps enhanced color size 9in,6in font "Helvetica,12" blacktext linewidth 2';
 			termsfx = '.eps';
@@ -23,7 +27,7 @@ endif
 			termtxt = 'postscript eps enhanced color size 4.5in,3in font "Helvetica,10" blacktext linewidth 2';
 			termsfx = '.eps';
 		otherwise
-			termtxt = "png size 1536,1024 truecolor linewidth 2";
+			termtxt = "png enhanced size 1536,1024 truecolor crop linewidth 2";
 			termsfx = '.png';
 	end
 end
