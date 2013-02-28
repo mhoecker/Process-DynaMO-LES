9b854359855c6e0fb12d91654646f12212838c36
Martin Hoecker-Martinez <mhoecker@coas.oregonstate,edu>
2013-02-27 21:42:08 -0800

    move windowing to harminic fit
    Added some comments and other changes

--- gappyspec.m
+++ gappyspec.m
@@ -1,5 +1,5 @@
-function [Fx,dFx,fgood,tgood,xgood,xfit,err] = gappyspec(x,t,win)
-# function [Fx,dFx,fgood,tgood,xgood,xfit,err] = gappyspec(x,t)
+function [Fx,dFx,f,xfit,err] = gappyspec(x,t,win)
+# function [Fx,dFx,f,tgood,xgood,xfit,err] = gappyspec(x,t)
 # Calculate the Fourier Transform of a gappy time series
 # 
 # Exrtacts a list of points where x is valid
@@ -9,13 +9,13 @@ function [Fx,dFx,fgood,tgood,xgood,xfit,err] = gappyspec(x,t,win)
 #
 # win --- is the choice of windowing function
 # win = 'sinw' uses the sine window (equivalent to band averaging two neighboring bands)
-#  sin(pi*(tgood-min(tgood))/GoodPeriod);
+#  sin(pi*(t-min(t))/Period);
 #
 # win = 'cosw' uses the cosine or Hann window (band averaging over 3 bands)
-#  (.5-cos(2*pi*(tgood-min(tgood))/GoodPeriod));
+#  (.5-cos(2*pi*(t-min(t))/Period));
 #
-# case 'cos2w' uses a double cosine window (band averagin vers 5 bands)
-#  xwin = xgood.*(3-4*cos(2*pi*(tgood-min(tgood))/GoodPeriod)+cos(4*pi*(tgood-min(tgood))/GoodPeriod))/8;
+# win = 'cos2w' uses a double cosine window (band averagin vers 5 bands)
+#  xwin = xgood.*(3-4*cos(2*pi*(tgood-min(t))/Period)+cos(4*pi*(t-min(t))/Period))/8;
 #
 # otherwise no windowing function is used
 #  
@@ -24,50 +24,48 @@ function [Fx,dFx,fgood,tgood,xgood,xfit,err] = gappyspec(x,t,win)
 #
 #
 #
+N = length(t);
 idxgood = find(~isnan(x));
-xgood = x(idxgood);
-tgood = t(idxgood);
 Ngood = length(idxgood);
-clear idxgood;
-#for testing only do the first 16 points
-# this will be deleted later
-#if(Ngood>32)
-# xgood = xgood(1:32);
-# tgood = tgood(1:32);
-# Ngood=32;
-#end
-errratio = 2;
-GoodPeriod = (max(tgood)-min(tgood))*(Ngood/(Ngood+1));
-df = 1/(GoodPeriod);
-switch(win)
- case 'sinw'
-  xwin = xgood.*sin(pi*(tgood-min(tgood))/GoodPeriod);
- case 'cosw'
-  xwin = xgood.*(.5-cos(2*pi*(tgood-min(tgood))/GoodPeriod));
- case 'cos2w'
-  xwin = xgood.*(3-4*cos(2*pi*(tgood-min(tgood))/GoodPeriod)+cos(4*pi*(tgood-min(tgood))/GoodPeriod))/8;
- otherwise
-  xwin = xgood;
-end
+Period = (max(t)-min(t))*(N/(N+1));
+df = 1/(Period);
 f = (1:floor((Ngood-1)/2))*df;
+# Initialize the output arrays to NaN
+Fx = f*NaN;
+dFx = f*NaN;
+xfit = x*NaN;
+errratio = .25;
+# Initialize empty arrays for fitting frequencies
 fgood = [];
 ftest = [];
+# Sucessively add frequencies to the fit
 for i=1:length(f)
+# add sucessfull frequencies and the next frequency to be tested
  ftest = [fgood,f(i)];
  Ntest = length(ftest);
- [A,dA] = harmonicfit(xwin,tgood,ftest);
+# Fit the current canidate list of frequencines
+ [A,dA] = harmonicfit(x,t,ftest,win);
  Fxtest = A(2*Ntest)+I*A(2*Ntest-1);
  dFxtest = dA(2*Ntest)+I*dA(2*Ntest-1);
+# Check if the current canidate is significant
  if((~isnan(Fxtest))&&(abs(2*dFxtest./Fxtest)<errratio/2));
-   Fx = [I*A(1:2:2*Ntest)+A(2:2:2*Ntest)];
-   dFx = [I*dA(1:2:2*Ntest)+dA(2:2:2*Ntest)];
-   idxok = find(abs(dFx./Fx)<errratio);
+# Check if the previously good frequencies are still significant
+   Fxgood = [A(2:2:2*Ntest)-I*A(1:2:2*Ntest)]/2;
+   dFxgood = [dA(2:2:2*Ntest)-I*dA(1:2:2*Ntest)]/2;
+   idxok = find(abs(dFxgood./Fxgood)<errratio);
+# Place surviving frequencies in the known good array
    fgood = ftest(idxok);
+ else
  end
 end
 Nf = length(fgood);
-figure(1)
-[A,dA,xfit,err] = harmonicfit(xwin,tgood,fgood);
-Fx = [I*A(1:2:2*Nf)+A(2:2:2*Nf)];
-dFx = [I*dA(1:2:2*Nf)+dA(2:2:2*Nf)];
+[A,dA,xfit,err] = harmonicfit(x,t,fgood,win);
+Fxgood = [A(2:2:2*Nf)-I*A(1:2:2*Nf)]/2;
+dFxgood = [dA(2:2:2*Nf)-I*dA(1:2:2*Nf)]/2;
+# Add NaNs for 'bad' frequencies
+for i=1:length(fgood)
+ idxfill = find(f==fgood(i));
+ Fx(idxfill) = Fxgood(i);
+ dFx(idxfill) = dFxgood(i);
+end
 
--- harmonicfit.m
+++ harmonicfit.m
@@ -1,9 +1,33 @@
-function [SCamp,dSCamp,y,err] = harmonicfit(x,t,f)
+function [SCamp,dSCamp,y,err] = harmonicfit(x,t,f,win)
+y = x*NaN;
+N = length(t);
+dt = mean(diff(t));
 idxgood = find(~isnan(x));
 xgood = x(idxgood);
+mu = mean(xgood);
+xgood = xgood-mu;
 tgood = t(idxgood);
 Ngood = length(idxgood);
+Period = (max(t)-min(t))*(N/(N+1));
 clear idxgood
+switch(win)
+ case 'sinw' # window using a sine function (jump rolls off like f^-2)
+  w = sin(pi*(tgood+dt/2-min(tgood))/Period);
+ case 'cosw' #window usind a hann window (jump rolls off like f^-4)
+  w = (.5-cos(2*pi*(tgood+dt/2-min(tgood))/Period));
+ case 'sin2w'# window using two sine functions (jump rolls off like f^-6)
+  w = (.75*sin(pi*(tgood+dt/2-min(tgood))/Period)-.25*sin(3*pi*(tgood+dt/2-min(tgood))/Period));
+ case 'cos2w'#window using three cosine functions (jump rolls off like f^-8)
+  w = (3-4*cos(2*pi*(tgood+dt/2-min(tgood))/Period)+cos(4*pi*(tgood+dt/2-min(tgood))/Period))/8;
+ otherwise
+  w = ones(size(xin));
+end
+# window the data
+nowinvar = var(xgood,1);
+xgood = w.*xgood;
+scale = nowinvar./var(xgood,1);
+xgood = scale*xgood;
+
 if(size(xgood)(1)!=1)
 xgood = xgood';
 end
@@ -15,18 +39,22 @@ Nt = length(tgood);
 SCval = zeros(Nt,2*Nf);
 for i=1:Nt
  for j=1:Nf
-  SCval(i,2*j-1) = sin(2*pi*f(j)*t(i));
-  SCval(i,2*j) = cos(2*pi*f(j)*t(i));
+  SCval(i,2*j-1) = sin(2*pi*f(j)*tgood(i));
+  SCval(i,2*j) = cos(2*pi*f(j)*tgood(i));
  end
 end
 SCvsq = SCval'*SCval;
 [SCvsi,rcon] = inv(SCvsq);
 if(rcon>eps*trace(SCvsi))
  SCamp = SCvsq\SCval'*xgood';
- y = SCamp'*SCval';
- err=xgood-y;
+ ygood = SCamp'*SCval';
+ err=xgood-ygood;
  sige = sum(err.*err)/(Ngood-2*Nf);
  dSCamp = sqrt(sige*diag(SCvsi)/Ngood);
+ for i=1:length(tgood)
+  idxfill = find(t==tgood(i));
+  y(idxfill) = (ygood(i)/scale+mu);
+ end
 else
  SCamp = NaN*ones(1,2*Nf);
  dSCamp = NaN*ones(1,2*Nf);
