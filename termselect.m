bfe4164c1547b9344d3ef3e36bf20df74f50d23a
Martin Hoecker-Martinez <mhoecker@coas.oregonstate,edu>
2013-02-28 21:02:01 -0800

    Correct help text for epsposter, add canvas and pngposter terms

--- termselect.m
+++ termselect.m
@@ -3,9 +3,11 @@ function [termtxt,termsfx] = termselect(termtype)
 # choose a consistent terminal type for gnuplot
 # the options are
 #
-# 		'epsposter' 15"x10" 48pt text
+# 		'epsposter' 12"x12" 48pt text
 #		'epsarticle' 9"x6" 12pt text
 #		'epsarticlesmall' 4.5"x3" 10pt font
+#		'pngposter' is intended to be blown up to 12"x12"
+#		'canvas' cerates an interactive web page
 #		
 #		if the term type is unrecognized the funtion returns values for a png
 #
@@ -17,7 +19,7 @@ endif
 			termtxt = 'postscript eps enhanced color size 12in,12in font "Helvetica,48" blacktext linewidth 2';
 			termsfx = '.eps';
 		case 'pngposter'
-			# if blown up to 12inx9in creates a 300dpi image
+			# if blown up to 12inx12in creates a 300dpi image
 			termtxt = 'png enhanced butt truecolor size 3600,3600 nocrop font "Helvetica,96" linewidth 6';
 			termsfx = '.png';
 		case 'epsarticle'
@@ -26,6 +28,9 @@ endif
 		case 'epsarticlesmall'
 			termtxt = 'postscript eps enhanced color size 4.5in,3in font "Helvetica,10" blacktext linewidth 2';
 			termsfx = '.eps';
+		case 'canvas'
+			termtxt = ' canvas size 1536,1024 jsdir "js" mousing enhanced linewidth 1';
+			termsfx = '.html';
 		otherwise
 			termtxt = "png enhanced size 1536,1024 truecolor nocrop linewidth 2";
 			termsfx = '.png';
