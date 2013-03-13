5fc193d3747227a9ddbd9bf2916c4274201fcc75
Martin Hoecker-Martinez <mhoecker@coas.oregonstate,edu>
2013-03-13 09:09:46 -0700

    make all fluxfigures for all levels

--- fluxfigure.m
+++ fluxfigure.m
@@ -12,7 +12,7 @@ ync = nc{'yu'}(:)';
 znc = nc{'zu'}(:);
 znc = ceil(max(znc))-znc;
 termtype = 'pngposter';
-for i=length(znc):length(znc)
+for i=1:length(znc)
 	output = ['/home/mhoecker/work/Dynamo/plots/run1/Tflux/HeatFlux' num2str(znc(i)) 'm'];
 	axeslabels = {'Upwind Dist. (m)','Crosswind Dist. (m)',['Heat Flux (W/m^2) at ' num2str(znc(i)) 'm Depth']};
 	z = squeeze(nc{'Tf'}(1,i,:,:))';
@@ -37,7 +37,7 @@ axeslabels = {
 'set linetype 1 lc rgb "#ffa0a0"',
 ['set arrow from ' num2str(minx/2) ',' num2str(minxy+7) ' to ' num2str(minx/2) ',' num2str(minxy-7) ' size character 2,20 lw 2 front filled'],
 ['set arrow from ' num2str(maxx/2) ',' num2str(maxxy-7) ' to ' num2str(maxx/2) ',' num2str(maxxy+7) ' size character 2,20 lw 2 front filled']
-}
+};
 myplot(output,x,y,axeslabels,termtype);
 clear axeslabels
 dz = ddz(y);
@@ -47,7 +47,7 @@ maxx = max(x);
 meany = mean(y);
 output = '/home/mhoecker/work/Dynamo/plots/run1/MeanHeatFluxDiv'
 axeslabels = {'Heat Flux Divergence (W/m^3)','Depth (m)','Divergence of Mean Turbulent Heat Flux','set style data filledcurves x1=0','set linetype 1 lc rgb "#a0a0ff"',['set label "Cooling" at ' num2str(minx/2) ',' num2str(meany) ' center front'],['set label "Warming" at ' num2str(maxx/2) ',' num2str(meany) ' center front']
-}
+};
 myplot(output,x,y,axeslabels,termtype);
 ncclose(nc);
 
