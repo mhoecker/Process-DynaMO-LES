0dfaadb4ff893791804b9b0c9761b165bf7335f1
Martin Hoecker-Martinez <mhoecker@coas.oregonstate,edu>
2013-04-26 12:18:57 -0700

    change filter so that is sub-samples
    If no filter width is given is assumes 4*the sub-sample spacing

--- csqfil.m
+++ csqfil.m
@@ -1,13 +1,29 @@
-function [y,fil] = csqfil(x,t,T)
+function [y] = csqfil(x,t,s,T)
+% [y,fil] = csqfil(x,t,T)
+% Filter the time series x with a weighting function
+%
+% weight(t,s) = (1+cos(2*pi*(s-t)/T))^2 for |s-t| < T/2
+% if T is not given T = 4*mean(diff(s))
+%
 N = length(t);
-fil = zeros(N,N);
-for i=1:N
+M = length(s);
+if(nargin<4)
+ T = 4*mean(diff(s));
+end%if
+y = zeros(1,M);
+for i=1:M
+ w = zeros(1,N);
  for j=1:N
-  if(abs(t(i)-t(j))<T/2)
-   fil(i,j) = (1+cos(2*pi*(t(i)-t(j))/T))^2;
+  if((abs(s(i)-t(j))<T/2)*(1-isnan(x(j))))
+   w(j) = (1+cos(2*pi*(s(i)-t(j))/T))^2;
+   y(i) = y(i)+x(j)*w(j);
   end%if
  end%for
-  fil(i,:) = fil(i,:)/sum(fil(i,:));
+  wsum = sum(w);
+  if(wsum==0)
+   y(i) = NaN;
+  else
+   y(i) = y(i)/wsum;
+  end%if
 end%for
-y = (x*fil);
 end%function
