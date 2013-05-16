719e139ffbcf68a8a7f64cf08fd4715c5f59c76b
Martin Hoecker-Martinez <mhoecker@coas.oregonstate,edu>
2013-05-16 15:56:12 -0700

    create TS initialization file for LES model

---
+++ LESinitialTS.m
@@ -0,0 +1,45 @@
+function fileout = LESinitialTS(fileloc,filename,wantdate,outloc)
+#function fileout = LESinitialTS(fileloc,filename,date,outloc)
+# fileloc  - directiory of the data file
+# filename - name of the data file (no suffix, it is assumed to be .nc)
+# wantdate - date in matlab datenum format
+# outloc   - directory of output file (assume to be the currentdirectory if not given)
+ if(nargin<4)
+  outloc = "";
+ end%if
+ gsw_data = 'gsw_data_v3_0.mat';
+ gsw_data_file = which(gsw_data);
+ load(gsw_data_file,'version_number','version_date');
+ gswloc = "/home/mhoecker/work/TEOS-10/";
+ gswlib = "/home/mhoecker/work/TEOS-10/library/";
+ ncfile = [fileloc filename ".nc"];
+ nc = netcdf(ncfile,"r");
+ t = nc{'th'}(:);
+ lat = nc{'lat'}(:);
+ lon = nc{'lon'}(:);
+ dateidx = find(abs(t-wantdate)==min(abs(t-wantdate)),1);
+ dateused  = t(dateidx);
+ clear t;
+ Tc = nc{'Tc'}(dateidx,:);
+ Sa = nc{'Sa'}(dateidx,:);
+ idxgood = find(isnan(Tc).*isnan(Sa)==0);
+ clear Sa Tc
+ z = nc{'z'}(idxgood);
+ Tc = nc{'Tc'}(dateidx,idxgood);
+ Sa = nc{'Sa'}(dateidx,idxgood);
+ P = nc{'P'}(idxgood);
+ Sp = gsw_SP_from_SA(Sa,P,lon,lat);
+ ncclose(nc);
+ #
+ fileout = [outloc int2str(floor(dateused)) filename ".ic"];
+ outid = fopen(fileout,"w");
+ fprintf(outid,'-z|Tc|Sp/%s.nc %sGMT lat %3.2f lon %3.2f\n',filename,datestr(dateused,"dd-mmm-yyyy HH"),lat,lon);
+ if(max(z)<0)
+  imax = find(z==max(z));
+  fprintf(outid,'%f %f %f\n',0.0,Tc(imax),Sp(imax))
+ end%if
+ for i=1:length(z);
+  fprintf(outid,'%f %f %f\n',abs(z(i)),Tc(i),Sp(i))
+ end%for
+ fclose(outid)
+end%function
