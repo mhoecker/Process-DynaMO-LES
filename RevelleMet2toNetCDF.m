19070283fad03dded1043f84eff6d04e4f5f1923
Martin Hoecker-Martinez <mhoecker@coas.oregonstate,edu>
2013-05-27 20:36:15 -0700

    change to use mat file instead of ascii for higher precission

--- RevelleMet2toNetCDF.m
+++ RevelleMet2toNetCDF.m
@@ -1,6 +1,6 @@
 function RevelleMet2toNetCDF(inloc,fname,outloc,tmp)
  %
- % This script converts RevelleMetRev2 acii data into
+ % This script converts RevelleMetRev2 mat data into
  % a CDl ascii file which it then uses `ncgen` to  convert the
  % CDL text file into a NetCDF file.  "ncgen" is a utility program
  % which is part of NetCDF which can be aquired here
@@ -16,28 +16,18 @@ function RevelleMet2toNetCDF(inloc,fname,outloc,tmp)
   tmp = "/home/mhoecker/tmp/";
   %tmp = [outloc "tmp"];
  end%if
- txtfile = [inloc  fname];
+ matfile = [inloc  fname '.mat'];
  ncfile  = [outloc fname '.nc' ];
  cdffile = [tmp    fname '.cdf'];
- X = load(txtfile);
- Ns = size(X);
- t = X(:,1);
- N = Ns(1);
- M = Ns(2);
+ load(matfile);
  cdlid = fopen(cdffile,'w');
  fprintf(cdlid,'netcdf %s {\n', fname);
+ vals = {};
  vars = {};
  longname = {};
  units = {};
  instrument = {};
  vartype = {};
- for i=1:M;
-  vars = {vars{:},['var' num2str(i)]};
-  units = {units{:},'TBD'};
-  longname = {longname{:},''};
-  instrument = {instrument{:},''};
-  vartype = {vartype{:},'double'};
- end%for
  %Yday      Decimal yearday (UTC) To convert to matlab time format use:
  % yd0=datenum(datestr('01-Jan-2011 00:00:00'))-1;
  % tmatlab=yday+yd0;
@@ -45,189 +35,330 @@ function RevelleMet2toNetCDF(inloc,fname,outloc,tmp)
  vars{i} = "Yday";
  units{i} = "days";
  longname{i} = "2011 year day (UTC)";
+ vartype{i} = 'double';
+ instrument{i} =  '';
+ vals{i} = yday;
+ clear yday;
  i = i+1;
  %Lat       Latitude (deg)
  vars{i} = "Lat";
  units{i} = "degrees";
  longname{i} = "Latitude";
+ vartype{i} = 'double';
+ instrument{i} =  '';
+ vals{i} = Lat;
+ clear Lat;
  i = i+1;
  %Lon       Longitude (deg)
  vars{i} = "Lon";
  units{i} = "degrees";
  longname{i} = "Longitude";
+ instrument{i} =  '';
+ vartype{i} = 'double';
+ vals{i} = Lon;
+ clear Lon;
  i = i+1;
  %SOG       Speed over ground (m/s)
  vars{i} = "SOG";
  units{i} = "meters/second";
  longname{i} = "Speed over ground";
+ instrument{i} =  '';
+ vartype{i} = 'double';
+ vals{i} = SOG;
+ clear SOG;
  i = i+1;
  %COG       Course over ground (deg)
  vars{i} = "COG";
  units{i} = "degrees from True North";
  longname{i} = "Course over ground";
+ instrument{i} =  '';
+ vartype{i} = 'double';
+ vals{i} = COG;
+ clear COG;
  i = i+1;
  %Heading   Ship's heading (deg)
  vars{i} = "Heading";
  units{i} = "degrees from True North";
  longname{i} = "Ship's heading";
+ instrument{i} =  '';
+ vartype{i} = 'double';
+ vals{i} = Heading;
+ clear Heading;
  i = i+1;
  %Cspd      Current speed (m/s)
  vars{i} = "Cspd";
  units{i} = "meters/second";
  longname{i} = "Current speed";
+ instrument{i} =  '';
+ vartype{i} = 'double';
+ vals{i} = cspd;
+ clear cspd;
  i = i+1;
  %Cdir      Current direction (deg) from
  vars{i} = "Cdir";
  units{i} = "degrees";
  longname{i} = "Current direction from";
+ instrument{i} =  '';
+ vartype{i} = 'double';
+ vals{i} = cdir;
+ clear cdir;
  i = i+1;
  %U10       Wind speed (m/s) relative to earth adjusted to 10 m
  vars{i} = "U10";
  units{i} = "meters/second";
  longname{i} = "Wind speed relative to earth adjusted to 10 m";
+ instrument{i} =  '';
+ vartype{i} = 'double';
+ vals{i} = U10;
+ clear U10;
  i = i+1;
  %Wdir      Wind direction (deg) from relative to earth
  vars{i} = "Wdir";
  units{i} = "degrees";
  longname{i} = "Wind direction from relative to earth";
+ instrument{i} =  '';
+ vartype{i} = 'double';
+ vals{i} = wdir;
+ clear wdir;
  i = i+1;
  %Ur10      Wind speed (m/s) relative to water adjusted to 10 m
  vars{i} = "Ur10";
  units{i} = "meters/second";
  longname{i} = "Wind speed relative to water adjusted to 10 m";
+ instrument{i} =  '';
+ vartype{i} = 'double';
+ vals{i} = Ur10;
+ clear Ur10;
  i = i+1;
  %WdirR     Wind direction (deg) from relative to water
  vars{i} = "WdirR";
  units{i} = "degrees";
  longname{i} = "Wind direction from relative to earth";
+ instrument{i} =  '';
+ vartype{i} = 'double';
+ vals{i} = wdirR;
+ clear wdirR;
  i = i+1;
  %Pair10    Pressure (mb) adjusted to 10 m
  vars{i} = "Pair10";
  units{i} = "millibar";
  longname{i} = "Pressure adjusted to 10 m";
+ instrument{i} =  '';
+ vartype{i} = 'double';
+ vals{i} = Pair10;
+ clear Pair10;
  i = i+1;
  %RH10      Relative humidity(%) adjusted to 10 m
  vars{i} = "RH10";
  units{i} = "Percentage";
  longname{i} = "Relative humidity adjusted to 10 m";
+ instrument{i} =  '';
+ vartype{i} = 'double';
+ vals{i} = RH10;
+ clear RH10;
  i = i+1;
  %T10       Temperature (C) adjusted to 10 m
  vars{i} = "T10";
  units{i} = "Celsius";
  longname{i} = "Temperature adjusted to 10 m";
+ instrument{i} =  '';
+ vartype{i} = 'double';
+ vals{i} = T10;
+ clear T10;
  i = i+1;
  %Tsea      Near surface sea temperature (C) from Sea snake
  vars{i} = "Tsea";
  units{i} = "Celsius";
  longname{i} = "Near surface sea temperature";
  instrument{i} =  "Primarily Sea snake, few values provided by the IR radiometer deployed by LDEO";
+ vartype{i} = 'double';
+ vals{i} = Tsea;
+ clear Tsea;
  i = i+1;
  %SST       Sea surface temperature (C) from Tsea minus cool skin
  vars{i} = "SST";
  units{i} = "Celsius";
  longname{i} = "Sea surface temperature from Tsea minus cool skin";
+ instrument{i} =  '';
+ vartype{i} = 'double';
+ vals{i} = SST;
+ clear SST;
  i = i+1;
  %Q10       Specific humidity (g/Kg) adjusted to 10 m
  vars{i} = "Q10";
  units{i} = "gram / kilogram";
  longname{i} = "Specific humidity adjusted to 10 m";
+ instrument{i} =  '';
+ vartype{i} = 'double';
+ vals{i} = Q10;
+ clear Q10;
  i = i+1;
  %Qsea      Specific humidity (g/Kg) 'near' ocean surface from sea snake
  vars{i} = "Qsea";
  units{i} = "gram / kilogram";
  longname{i} = "Specific humidity 'near' ocean surface";
  instrument{i} =  "Sea snake";
+ vartype{i} = 'double';
+ vals{i} = Qsea;
+ clear Qsea;
  i = i+1;
  %SSQ       Sea surface specific humidity (g/Kg) from Qsea minus cool skin
  vars{i} = "SSQ";
  units{i} = "gram / kilogram";
  longname{i} = "Sea surface specific humidity from Qsea minus cool skin";
+ instrument{i} =  '';
+ vartype{i} = 'double';
+ vals{i} = SSQ;
+ clear SSQ;
  i = i+1;
  %stress    Surface stress (N/m2) measured relative to water
  vars{i} = "stress";
  units{i} = "Newton / meter^2";
  longname{i} = "Surface stress measured relative to water";
+ instrument{i} =  '';
+ vartype{i} = 'double';
+ vals{i} = stress;
+ clear stress;
  i = i+1;
  %shf       Sensible heat flux (W/m2)
  vars{i} = "shf";
  units{i} = "Watt / meter^2";
  longname{i} = "Sensible heat flux";
+ instrument{i} =  '';
+ vartype{i} = 'double';
+ vals{i} = shf;
+ clear shf;
  i = i+1;
  %lhf       Latent heat flux (W/m2)
  vars{i} = "lhf";
  units{i} = "Watt / meter^2";
  longname{i} = "Latent heat flux";
+ instrument{i} =  '';
+ vartype{i} = 'double';
+ vals{i} = lhf;
+ clear lhf;
  i = i+1;
  %rhf       Sensible heat flux from rain (W/m2)
  vars{i} = "rhf";
  units{i} = "Watt / meter^2";
  longname{i} = "Sensible heat flux from rain";
+ instrument{i} =  '';
+ vartype{i} = 'double';
+ vals{i} = rhf;
+ clear rhf;
  i = i+1;
  %Solarup   Reflected solar (W/m2) estimated from Payne (1972)
  vars{i} = "Solarup";
  units{i} = "Watt / meter^2";
  longname{i} = "Reflected solar estimated from Payne (1972)";
+ instrument{i} =  '';
+ vartype{i} = 'double';
+ vals{i} = Solarup;
+ clear Solarup;
  i = i+1;
  %Solardn   Measured downwelling solar (W/m2)
  vars{i} = "Solardn";
  units{i} = "Watt / meter^2";
  longname{i} = "Measured downwelling solar";
+ instrument{i} =  '';
+ vartype{i} = 'double';
+ vals{i} = Solardn;
+ clear Solardn;
  i = i+1;
  %IRup      Upwelling IR (W/m2) computed from SST
  vars{i} = "IRup";
  units{i} = "Watt / meter^2";
  longname{i} = "Upwelling IR computed from SST";
+ instrument{i} =  '';
+ vartype{i} = 'double';
+ vals{i} = IRup;
+ clear IRup;
  i = i+1;
  %IRdn      Measured downwelling IR (W/m2)
  vars{i} = "IRdn";
  units{i} = "Watt / meter^2";
  longname{i} = "Measured downwelling IR";
+ instrument{i} =  '';
+ vartype{i} = 'double';
+ vals{i} = IRdn;
+ clear IRdn;
  i = i+1;
  %E         Evaporation rate (mm/hr)
  vars{i} = "E";
  units{i} = "milimeter / hour";
  longname{i} = "Evaporation rate";
+ instrument{i} =  '';
+ vartype{i} = 'double';
+ vals{i} = E;
+ clear E;
  i = i+1;
  %P         Precipitation rate (mm/hr)
  vars{i} = "P";
  units{i} = "milimeter / hour";
  longname{i} = "Precipitation rate";
+ instrument{i} =  '';
+ vartype{i} = 'double';
+ vals{i} = P;
+ clear P;
  i = i+1;
  %Evap      Accumulated evaporation for Leg (mm)
  vars{i} = "Evap";
  units{i} = "milimeter";
  longname{i} = "Accumulated evaporation for Leg";
+ vartype{i} = 'double';
+ instrument{i} =  '';
+ vals{i} = Evap;
+ clear Evap;
  i = i+1;
  %Precip    Accumulated precipitation for Leg (mm)
  vars{i} = "Precip";
  units{i} = "milimeter";
  longname{i} = "Accumulated precipitation for Leg";
+ instrument{i} =  '';
+ vartype{i} = 'double';
+ vals{i} = Precip;
+ clear Precip;
  i = i+1;
  %Interped  1= interpolated due to poor relative winds 0=no interpolation
  vars{i} = "Interped";
  units{i} = "N/A";
  longname{i} = "0=no interpolation, 1=interpolated due to poor relative winds";
+ instrument{i} =  '';
  vartype{i} = "byte";
+ vals{i} = interped;
+ clear interped;
  i = i+1;
  %T02       Temperature (C) adjusted to 2 m
  vars{i} = "T02";
  units{i} = "Celsius";
  longname{i} = "Temperature adjusted to 2 m";
+ instrument{i} =  '';
+ vartype{i} = 'double';
+ vals{i} = T02;
+ clear T02;
  i = i+1;
  %Q02       Specific humidity (g/Kg) adjusted to 2 m
  vars{i} = "Q02";
  units{i} = "gram / kilogram";
  longname{i} = "Specific humidity adjusted to 2 m";
+ instrument{i} =  '';
+ vartype{i} = 'double';
+ vals{i} = Q02;
+ clear Q02;
  i = i+1;
  %RH02      Relative humidity(%) adjusted to 2 m
  vars{i} = "RH02";
  units{i} = "percent";
  longname{i} = "Relative humidity adjusted to 2 m";
+ instrument{i} =  '';
+ vartype{i} = 'double';
+ vals{i} = RH02;
+ clear RH02;
  % Declare Dimensions
- fprintf(cdlid,'dimensions:\n %s=%i;\n',vars{1},N);
+ fprintf(cdlid,'dimensions:\n %s=%i;\n',vars{1},length(vals{1}));
  % Declare variables with units, long_name and instrument
  fprintf(cdlid,'variables:\n');
- for i=1:M
+ for i=1:length(vars)
   fprintf(cdlid,'%s %s(%s);\n',vartype{i},vars{i},vars{1});
   fprintf(cdlid,'%s:units = "%s";\n',vars{i},units{i});
   fprintf(cdlid,'%s:long_name = "%s";\n',vars{i},longname{i});
@@ -245,17 +376,16 @@ function RevelleMet2toNetCDF(inloc,fname,outloc,tmp)
  fprintf(cdlid,':Notes = "%s";\n',notes)
  # Declare Data
  fprintf(cdlid,'data:\n')
- Y = X;
- for i=1:M
-  y = X(:,i);
+ for i=1:length(vals)
+  y = vals{i};
   fprintf(cdlid,'%s =\n',vars{i});
-  for j=1:N
+  for j=1:length(y)
    if(isnan(y(j))==1)
     fprintf(cdlid,'NaN');
    else
     fprintf(cdlid,'%f',y(j));
    endif
-   if(j<N)
+   if(j<length(y))
     fprintf(cdlid,', ');
    else
     fprintf(cdlid,';\n');
