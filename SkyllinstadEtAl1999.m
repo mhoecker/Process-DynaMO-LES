59a1186a1b7220ae6029ef6e9011f15395e5292a
Martin Hoecker-Martinez <mhoecker@coas.oregonstate,edu>
2013-09-11 11:48:31 -0700

    Function to recreate figures in Skyllinstad et al 1999 (start)
    Begin file

---
+++ SkyllinstadEtAl1999.m
@@ -0,0 +1,22 @@
+function SkyllinstadEtAl1999(dagnc,sfxnc,chmnc,outdir)
+% function skyllinstad1999(dagnc,sfxnc,chmnc,outdir)
+% dagnc - dag file from LES (netCDF)
+% sfxnc - surface flux file (netCDF)
+% chmnc - Chameleon data file (netCDF)
+%
+% Recreate the figures in
+% Skyllingstad, E. D.; Smyth, W. D.; Moum, J. N. & Wijesekera, H.
+% Upper-ocean turbulence during a westerly wind burst: A comparison of large-eddy simulation results and microstructure measurements
+% Journal of physical oceanography, 1999, 29, 5-28
+%
+end%function
+
+% figure 1
+function fig1(sfxnc,chmnc)
+end%function
+
+%figure 2
+
+%figure 3
+
+%figure 4
