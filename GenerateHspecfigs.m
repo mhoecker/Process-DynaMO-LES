3498c02fcb0b3349f394f880d661779092626bfd
Martin Hoecker-Martinez <mhoecker@coas.oregonstate,edu>
2013-09-11 11:02:40 -0700

    invoke Hspec on a bunch of restart files

---
+++ GenerateHspecfigs.m
@@ -0,0 +1,24 @@
+plotdir = "/home/mhoecker/work/Dynamo/plots/";
+datsdir = "/home/mhoecker/work/Dynamo/output/";
+% run 7
+%Hspectra([ datsdir "run7/dyno_328Rev_4-a_7200_rst.nc"],[ plotdir "run7/dissipation/7200"]);
+%Hspectra([ datsdir "run7/dyno_328Rev_4-a_14400_rst.nc"],[ plotdir "run7/dissipation/14400"]);
+%Hspectra([ datsdir "run7/dyno_328Rev_4-a_21600_rst.nc"],[ plotdir "run7/dissipation/21600"]);
+%Hspectra([ datsdir "run7/dyno_328Rev_4-a_28800_rst.nc"],[ plotdir "run7/dissipation/28800"]);
+%Hspectra([ datsdir "run7/dyno_328Rev_4-a_36000_rst.nc"],[ plotdir "run7/dissipation/36000"]);
+%Hspectra([ datsdir "run7/dyno_328Rev_4-a_43200_rst.nc"],[ plotdir "run7/dissipation/43200"]);
+%Hspectra([ datsdir "run7/dyno_328Rev_4-a_50400_rst.nc"],[ plotdir "run7/dissipation/50400"]);
+%Hspectra([ datsdir "run7/dyno_328Rev_4-a_57600_rst.nc"],[ plotdir "run7/dissipation/57600"]);
+%Hspectra([ datsdir "run7/dyno_328Rev_4-a_64800_rst.nc"],[ plotdir "run7/dissipation/64800"]);
+%Hspectra([ datsdir "run7/dyno_328Rev_4-a_72000_rst.nc"],[ plotdir "run7/dissipation/72000"]);
+%Hspectra([ datsdir "run7/dyno_328Rev_4-a_79200_rst.nc"],[ plotdir "run7/dissipation/79200"]);
+%Hspectra([ datsdir "run7/dyno_328Rev_4-a_86400_rst.nc"],[ plotdir "run7/dissipation/86400"]);
+%Hspectra([ datsdir "run7/dyno_328Rev_4-b_108000_rst.nc"],[ plotdir "run7/dissipation/108000"]);
+%Hspectra([ datsdir "run7/dyno_328Rev_4-b_129600_rst.nc"],[ plotdir "run7/dissipation/129600"]);
+%Hspectra([ datsdir "run7/dyno_328Rev_4-b_151200_rst.nc"],[ plotdir "run7/dissipation/151200"]);
+%
+% run 8
+%/home/mhoecker/work/Dynamo/output/run8/dyno_328Rev_5-a_21600_rst.nc
+Hspectra([ datsdir "run8/dyno_328Rev_5-a_64800_rst.nc"],[ plotdir "run8/dissipation/64800"]);
+Hspectra([ datsdir "run8/dyno_328Rev_5-a_43200_rst.nc"],[ plotdir "run8/dissipation/43200"]);
+Hspectra([ datsdir "run8/dyno_328Rev_5-a_21600_rst.nc"],[ plotdir "run8/dissipation/21600"]);
