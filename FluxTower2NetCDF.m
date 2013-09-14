7ecd71f800429df701ead787c1b71259a4b29d9a
Martin Hoecker-Martinez <mhoecker@coas.oregonstate,edu>
2013-09-13 17:20:12 -0700

    change source from ascii to mat file, invoke writeCDFdata

--- FluxTower2NetCDF.m
+++ FluxTower2NetCDF.m
@@ -1,6 +1,6 @@
 function FluxTower2NetCDF(inloc,fname,outloc)
 %%
-%% This script converts the raw 32 columns of acii data into
+%% This script converts the .mat data into
 %% a CDl ascii file it then uses `ncgen` to  convert the
 %% CDL text file into a NetCDF file.  "ncgen" is a utility program
 %% which is part of NetCDF which can be aquired here
@@ -12,16 +12,12 @@ function FluxTower2NetCDF(inloc,fname,outloc)
 %% This program is provided with no guarntees whatsoever
 %%
 %%
-txtfile = [inloc fname,'.txt'];
+matfile = [inloc fname,'.mat'];
 ncfile = [outloc fname '.nc'];
 tmp = "/home/mhoecker/tmp/";
 cdffile = [tmp fname '.cdf'];
 #
-X = load(txtfile);
-Ns = size(X);
-t = X(:,1);
-N = Ns(1);
-M = Ns(2);
+load(matfile);
 #
 cdlid = fopen(cdffile,'w');
 fprintf(cdlid,'netcdf %s {\n', fname);
@@ -30,173 +26,154 @@ vars = {};
 longname = {};
 units = {};
 instrument = {};
-for i=1:M;
- vars = {vars{:},['var' num2str(i)]};
- units = {units{:},'TBD'};
- longname = {longname{:},''};
- instrument = {instrument{:},''};
-endfor
-vars{1} = "t";
-units{1} = "days";
-longname{1} = "2011 year day at start of average";
+val = {};
+k=0;
 #
-vars{2} = "WS";
-units{2} = "meter/second";
-longname{2} = "Wind Speed";
-instrument{2} = "PSD sonic";
+k=k+1;
+vars = {vars{:},"yday"};
+units = {units{:},"day"};
+longname = {longname{:},"2011 year day at start of average"};
+instrument = {instrument{:},""};
+val = {val{:},PSDfx.time(:)-datenum(2011,1,0)};
 #
-vars{3} = "WH";
-units{3} = "degrees";
-longname{3} = "Wind Heading Direction wind comes from measured clocwise from True North";
-instrument{3} = "PSD sonic";
+k=k+1;
+vars = {vars{:},"U10"};
+units = {units{:},"meter/second"};
+longname = {longname{:},"Wind Speed at 10m, earth relative"};
+instrument = {instrument{:},""};
+val = {val{:},PSDfx.U10(:)};
 #
-vars{4} = "SST";
-units{4} = "Celcius";
-longname{4} = "Sea Surface Temperature";
-instrument{4} = "Sea Snake";
+k=k+1;
+vars = {vars{:},"wdir"};
+units = {units{:},"degrees"};
+longname = {longname{:},"Direction earth relative wind comes from measured clocwise from True North"};
+instrument = {instrument{:},""};
+val = {val{:},PSDfx.wdir(:)};
 #
-vars{5} = "T";
-units{5} = "Celcius";
-longname{5} = "Temperature";
-instrument{5} = "TSG";
+k=k+1;
+vars = {vars{:},"SST"};
+units = {units{:},"Celcius"};
+longname = {longname{:},"Sea Surface Temperature"};
+instrument = {instrument{:}, "Sea Snake - cool skin"};
+val = {val{:},PSDfx.SST(:)};
 #
-vars{6} = "S";
-units{6} = "PSU";
-longname{6} = "Salinity";
-instrument{6} = "TSG";
+k=k+1;
+vars = {vars{:},"Tsea"};
+units = {units{:},"Celcius"};
+longname = {longname{:},"Temperature"};
+instrument = {instrument{:}, "Sea Snake"};
+val = {val{:},PSDfx.Tsea(:)};
 #
-vars{7} = "AT";
-units{7} = "Celcius";
-longname{7} = "Air Temperature";
-instrument{7} = "PSD";
+k=k+1;
+vars = {vars{:},"T10"};
+units = {units{:},"Celcius"};
+longname = {longname{:},"Air Temperature"};
+instrument = {instrument{:},""};
+val = {val{:},PSDfx.T10(:)};
 #
-vars{8} = "SSQ";
-units{8} = "gram/kilogram";
-longname{8} = "Sea Surface specific humidity";
-instrument{8} = "Sea Snake";
+k=k+1;
+vars = {vars{:},"SSQ"};
+units = {units{:},"gram/kilogram"};
+longname = {longname{:},"Sea Surface specific humidity"};
+instrument = {instrument{:}, "From Qsea - cool skin"};
+val = {val{:},PSDfx.SSQ(:)};
 #
-vars{9} = "AQ";
-units{9} = "gram/kilogram";
-longname{9} = "Air specific humidity";
-instrument{9} = "PSD";
+k=k+1;
+vars = {vars{:},"Qsea"};
+units = {units{:},"gram/kilogram"};
+longname = {longname{:},"Near surface specific humidity"};
+instrument = {instrument{:},"Sea Snake"};
+val = {val{:},PSDfx.Qsea(:)};
 #
-vars{10} = "SWRF";
-units{10} = "Watt/meter^2";
-longname{10} = "net Short Wave Radiation Flux + = downward (correct by multiplying by 1.01)";
-instrument{10} = "PSD";
+k=k+1;
+vars = {vars{:},"Solardn"};
+units = {units{:},"Watt/meter^2"};
+longname = {longname{:},"Downward Short Wave Radiation Flux"};
+instrument = {instrument{:},""};
+val = {val{:},PSDfx.Solardn(:)};
 #
-vars{11} = "LWRF";
-units{11} = "Watt/meter^2";
-longname{11} = "incident Long Wave Radiation Flux + = downward";
-instrument{11} = "PSD";
+k=k+1;
+vars = {vars{:},"IRdn"};
+units = {units{:},"Watt/meter^2"};
+longname = {longname{:},"Downward Long Wave Radiation Flux"};
+instrument = {instrument{:},""};
+val = {val{:},PSDfx.IRdn(:)};
 #
-vars{12} = "PR";
-units{12} = "mm/hour";
-longname{12} = "uncorrected Precipitation Rate";
-instrument{12} = "PSD STI optical rain gauge";
+k=k+1;
+vars = {vars{:},"P"};
+units = {units{:},"mm/hour"};
+longname = {longname{:},"Precipitation Rate"};
+instrument = {instrument{:}, "PSD STI optical rain gauge"};
+val = {val{:},PSDfx.P(:)};
 #
-vars{13} = "VS";
-units{13} = "m/s";
-longname{13} = "Vessel Speed";
-instrument{13} = "SCS";
+k=k+1;
+vars = {vars{:},"SOG"};
+units = {units{:},"m/s"};
+longname = {longname{:},"Speed over gound"};
+instrument = {instrument{:},""};
+val = {val{:},PSDfx.SOG(:)};
 #
-vars{14} = "VH";
-units{14} = "degrees";
-longname{14} = "Vessel Heading measured clocwise from True North";
-instrument{14} = "SCS";
+k=k+1;
+vars = {vars{:},"COG"};
+units = {units{:},"degrees"};
+longname = {longname{:},"Course over gound (clocwise from true North)"};
+instrument = {instrument{:},""};
+val = {val{:},PSDfx.COG(:)};
 #
-vars{15} = "RWS";
-units{15} = "m/s";
-longname{15} = "Relative Wind Speed";
-instrument{15} = "PSD";
+k=k+1;
+vars = {vars{:},"Ur10"};
+units = {units{:},"m/s"};
+longname = {longname{:},"Wind Speed at 10m, water relative"};
+instrument = {instrument{:},""};
+val = {val{:},PSDfx.Ur10(:)};
 #
-vars{16} = "RWH";
-units{16} = "degrees";
-longname{16} = "Relative Wind Heading Direction wind comes from measured clocwise from the bow";
-instrument{16} = "PSD sonic";
+k=k+1;
+vars = {vars{:},"wdirR"};
+units = {units{:},"degrees"};
+longname = {longname{:},"Wind Direction, water relative (clocwise from true North)"};
+instrument = {instrument{:},""};
+val = {val{:},PSDfx.wdirR(:)};
 #
-vars{17} = "lat";
-units{17} = "degrees";
-longname{17} = "Latitude";
-instrument{17} = "SCS pcode";
+k=k+1;
+vars = {vars{:},"Lat"};
+units = {units{:},"degrees"};
+longname = {longname{:},"Latitude"};
+instrument = {instrument{:},""};
+val = {val{:},PSDfx.Lat(:)};
 #
-vars{18} = "lon";
-units{18} = "degrees";
-longname{18} = "Longitude";
-instrument{18} = "SCS pcode";
+k=k+1;
+vars = {vars{:},"stress"};
+units = {units{:},"Newton/meter^2"};
+longname = {longname{:},"bulk wind stress magnitude"};
+instrument = {instrument{:},""};
+val = {val{:},PSDfx.stress(:)};
 #
-vars{19} = "Zref";
-units{19} = "m";
-longname{19} = "Depth for bulk flux Ts reference";
-instrument{19} = "SCS pcode";
+k=k+1;
+vars = {vars{:},"shf"};
+units = {units{:},"Watt/meter^2"};
+longname = {longname{:},"Sensible heat Flux"};
+instrument = {instrument{:},""};
+val = {val{:},PSDfx.shf(:)};
 #
-vars{20} = "VSsd";
-units{20} = "m/s";
-longname{20} = "standard deviation of Vessel Speed";
-instrument{20} = "SCS pcode";
+k=k+1;
+vars = {vars{:},"lhf"};
+units = {units{:},"Watt/meter^2"};
+longname = {longname{:},"Latent heat Flux"};
+instrument = {instrument{:},""};
+val = {val{:},PSDfx.lhf(:)};
 #
-vars{21} = "Tau";
-units{21} = "Newton/meter^2";
-longname{21} = "bulk wind stress magnitude";
+k=k+1;
+vars = {vars{:},"rhf"};
+units = {units{:},"Watt/meter^2"};
+longname = {longname{:},"Rain sensible heat Flux"};
+instrument = {instrument{:},""};
+val = {val{:},PSDfx.rhf(:)};
 #
-vars{22} = "SF";
-units{22} = "Watt/meter^2";
-longname{22} = "Sensible heat Flux";
-#
-vars{23} = "LF";
-units{23} = "Watt/meter^2";
-longname{23} = "Latent heat Flux";
-#
-vars{24} = "RainF";
-units{24} = "Watt/meter^2";
-longname{24} = "Rain heat Flux";
-#
-vars{25} = "TA2";
-units{25} = "Celcius";
-longname{25} = "Air Temperature";
-instrument{25} = "IMET";
-#
-vars{26} = "AQ2";
-units{26} = "gram/kilogram";
-longname{26} = "Air specific humidity";
-instrument{26} = "IMET";
-#
-vars{27} = "VSOG";
-units{27} = "m/s";
-longname{27} = "Vessel Speed Over Ground";
-#
-vars{28} = "VH2";
-units{28} = "degrees";
-longname{28} = "Vessel Heading measured clocwise from True North";
-instrument{28} = "gyro";
-#
-vars{29} = "SWRF2";
-units{29} = "Watt/meter^2";
-longname{29} = "Solar flux";
-instrument{29} = "ship pyranometer";
-#
-vars{30} = "LWRF2";
-units{30} = "Watt/meter^2";
-longname{30} = "Infrared flux";
-instrument{30} = "ship pirgeometer";
-#
-vars{31} = "P";
-units{31} = "hectopascal";
-longname{31} = "barometric Pressure";
-instrument{31} = "IMET";
-#
-vars{32} = "RH";
-units{32} = "";
-longname{32} = "RHvais";
-instrument{32} = "PSD";
-#
-%vars
-%units
 # Declare Dimensions
-fprintf(cdlid,'dimensions:\n %s=%i;\n',vars{1},N);
+fprintf(cdlid,'dimensions:\n %s=%i;\n',vars{1},length(val{1}(:)));
 # Declare variables
 fprintf(cdlid,'variables:\n');
-for i=1:M
+for i=1:k
  fprintf(cdlid,'double %s(%s);\n',vars{i},vars{1});
 # Add units, long_name and instrument
  fprintf(cdlid,'\n');
@@ -207,7 +184,7 @@ for i=1:M
  endif
 endfor
 #Declare global attributes
-fprintf(cdlid,':source = "%s";\n',[fname '.txt']);
+fprintf(cdlid,':source = "%s";\n',[fname '.mat']);
 fprintf(cdlid,':vessel = "R/V Roger Revelle";\n');
 fprintf(cdlid,':Flux_Algorithm = "bulk COARE version 3.0";\n');
 fprintf(cdlid,':LW_Flux = "Fairall et al. Jtech, 1998.";\n');
@@ -218,25 +195,24 @@ fprintf(cdlid,':PSD_sonic_Height = "18.5 meters";\n');
 fprintf(cdlid,':IMET_Height = "21 meters";\n');
 
 
-# Declare Data
-fprintf(cdlid,'data:\n')
-Y = X;
-for i=1:M
- y = X(:,i);
- fprintf(cdlid,'%s =\n',vars{i});
- for j=1:N
-  if(isnan(y(j))==1)
-   fprintf(cdlid,'NaN');
-  else
-   fprintf(cdlid,'%20.20g',y(j));
-  endif
-  if(j<N)
-   fprintf(cdlid,', ');
-  else
-   fprintf(cdlid,';\n');
-  endif
- endfor
-endfor
-fprintf(cdlid,'}\n', fname)
-fclose(cdlid)
+writeCDFdata(cdlid,val,vars)
+#fprintf(cdlid,'data:\n')
+#for i=1:M
+# y = X(:,i);
+# fprintf(cdlid,'%s =\n',vars{i});
+# for j=1:N
+#  if(isnan(y(j))==1)
+#   fprintf(cdlid,'NaN');
+#  else
+#   fprintf(cdlid,'%20.20g',y(j));
+#  endif
+#  if(j<N)
+#   fprintf(cdlid,', ');
+#  else
+#   fprintf(cdlid,';\n');
+#  endif
+# endfor
+#endfor
+#fprintf(cdlid,'}\n', fname)
+#fclose(cdlid)
 unix(['ncgen -o ' ncfile ' ' cdffile ' && rm ' cdffile])
