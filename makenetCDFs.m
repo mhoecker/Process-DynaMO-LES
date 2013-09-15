91e927919eedab4c18f983390680c80f10bd47b8
Martin Hoecker-Martinez <mhoecker@coas.oregonstate,edu>
2013-09-15 15:00:56 -0700

    add some semicolons to suppress output

--- makenetCDFs.m
+++ makenetCDFs.m
@@ -1,7 +1,7 @@
 function makenetCDFs()
 ObsDir = '/media/mhoecker/8982053a-3b0f-494e-84a1-98cdce5e67d9/Dynamo/Observations/';
-matdir = 'mat/'
-netcdfdir = 'netCDF/'
+matdir = 'mat/';
+netcdfdir = 'netCDF/';
 % Convert the flux data
 % /media/mhoecker/8982053a-3b0f-494e-84a1-98cdce5e67d9/Dynamo/Observations/mat/FluxTower/PSDflx_leg3.mat
 fluxdir = 'FluxTower/';
