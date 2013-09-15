d7cd9fbdfb375c0db92f755170d326ea2dfed90b
Martin Hoecker-Martinez <mhoecker@coas.oregonstate,edu>
2013-09-15 13:48:09 -0700

    add semicolons

--- makenetCDFs.m
+++ makenetCDFs.m
@@ -6,16 +6,16 @@ netcdfdir = 'netCDF/'
 % /media/mhoecker/8982053a-3b0f-494e-84a1-98cdce5e67d9/Dynamo/Observations/mat/FluxTower/PSDflx_leg3.mat
 fluxdir = 'FluxTower/';
 fluxfile = 'PSDflx_leg3';
-FluxTower2NetCDF([ObsDir matdir fluxdir],fluxfile,[ObsDir netcdfdir fluxdir])
+FluxTower2NetCDF([ObsDir matdir fluxdir],fluxfile,[ObsDir netcdfdir fluxdir]);
 %
 % Convert ADCP data
 adcpdir = 'ADCP/';
 adcpfile = 'adcp150_filled_with_140';
-adcp2NetCDF([ObsDir matdir adcpdir],adcpfile,[ObsDir netcdfdir adcpdir])
+adcp2NetCDF([ObsDir matdir adcpdir],adcpfile,[ObsDir netcdfdir adcpdir]);
 %
 % Convert Chameloen data
 chamdir = 'Chameleon/';
 chamfile = 'dn11b_sum_clean_v2';
-cham2NetCDF([ObsDir matdir chamdir],chamfile,[ObsDir netcdfdir chamdir])
+cham2NetCDF([ObsDir matdir chamdir],chamfile,[ObsDir netcdfdir chamdir]);
 
 end%function
