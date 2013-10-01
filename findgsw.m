6f6a8d876dcd3b2db0f3bf177e10899160c9c2bb
Martin Hoecker-Martinez <mhoecker@coas.oregonstate,edu>
2013-10-01 13:32:01 -0700

    oops, check if GibbsSeaWater directories exist are in path

--- findgsw.m
+++ findgsw.m
@@ -1,10 +1,10 @@
 function findgsw
  gswpath = "/home/mhoecker/work/TEOS-10/";
  gswlibpath = "/home/mhoecker/work/TEOS-10/library/";
- if(exitst(gswpath,"dir")!=7)
+ if((exist(gswpath,"dir")==7).*(length(findstr(path,gswpath))==0))
   addpath(gswpath);
  end%if
- if(exist(gswlibpath,"dir")!=7)
+ if((exist(gswlibpath,"dir")==7).*(length(findstr(path,gswlibpath))==0))
   addpath(gswlibpath);
  end%if
 end%function
