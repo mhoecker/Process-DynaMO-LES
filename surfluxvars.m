0db01a5ab4aa35613a24180a0fbddf2f603f5de9
Martin Hoecker-Martinez <mhoecker@coas.oregonstate,edu>
2013-10-09 16:20:15 -0700

    make a generic extractor of surface flux variables
    reads a list of fields and a dimension range and returns the variables
    in a structure

---
+++ surfluxvars.m
@@ -0,0 +1,22 @@
+function vars = surfluxvars(sfxnc,field,trange)
+ % Extract Flux data from netCDF file
+ % vars = surfluxvars(sfxnc,fields,trange)
+ % vars.(field(i)) =  field i for field(1) in trange
+ % if trange is not given then th whole data record is used
+ % if feilds is not given "Yday" is used
+ sfx = netcdf(sfxnc,'r');
+ if(nargin<1)
+  field = ["Yday"];
+ end%if
+ % get the dimension variable
+ tsfx = squeeze(sfx{field(1,:)}(:));
+ if nargin()>2
+  sfxtidx = inclusiverange(tsfx,trange);
+ else
+  sfxtidx = 1:length(tsfx);
+ end%if
+ for i=1:length(field(:,1))
+  vars.(field(i,:)) = squeeze(sfx{field(i,:)}(sfxtidx));
+ end%for
+ ncclose(sfx);
+end%function
