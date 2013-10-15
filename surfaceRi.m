0544654949c39157b7195f7c729153e4e931c8aa
Martin Hoecker-Martinez <mhoecker@coas.oregonstate,edu>
2013-10-15 08:34:42 -0700

    use JhtoJb to onver heat to buoyancy flux

--- surfaceRi.m
+++ surfaceRi.m
@@ -1,18 +1,20 @@
-function [Ri,alpha,g,nu,kappaT] = surfaceRi(stress,Jh,sst,sss)
+function [Ri,Jb,rho0,alpha,g,sssa,cp,nu,kappaT] = surfaceRi(stress,Jh,sst,sss,lat,lon)
  % Convert surface stress and heat flux into a richardson number
  %
- % Ri = g * alpha * ( Jh / kappaT ) * / (stress/nu rho)^2
+ % Ri = Jb / (stress/nu rho)^2
  %
- findgsw
+ if(nargin()<5)
+  lat = 0;
+ end%if
+ if(nargin()<6)
+  lon = 80.5;
+ end%if
  nu = 1.05e-6; %
  kappaT = 1.46e-7; %
  % from
  % Tables of Physical & Chemical Constants (16th edition 1995).
  % 2.7.9 Physical properties of sea water.
  % Kaye & Laby Online. Version 1.0 (2005) www.kayelaby.npl.co.uk
- sssa = gsw_SA_from_SP(sss,80.5+0*sss,0*sss,0*sss);
- alpha = gsw_alpha(sst,sssa,0*sst );
- rho0 = gsw_rho(sst,sssa,0*sst);
- g = grav = gsw_grav(0);
- Ri = -(nu.^2).*g.*rho0.*alpha.*Jh./(kappaT.*stress.^2);
+ [Jb,rho0,sssa,cp,alpha,g,kappaT] = JhtoJb(Jh,sst,sss,lat,lon);
+ Ri = (Jb./kappaT)./((stress./(nu.*rho0)).^2);
 end%function
