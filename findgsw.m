b516d650391be8a34c48a64cd643994df6bcdacb
Martin Hoecker-Martinez <mhoecker@coas.oregonstate,edu>
2013-10-16 12:07:45 -0700

    add Gibbs Sea Water version info to findgsw output

--- findgsw.m
+++ findgsw.m
@@ -1,4 +1,6 @@
-function findgsw
+function [version_number,version_date] = findgsw
+ % Ensure Gibbs sea water is in the path,
+ % return the version number and date
  gswpath = "/home/mhoecker/work/TEOS-10/";
  gswlibpath = "/home/mhoecker/work/TEOS-10/library/";
  if((exist(gswpath,"dir")==7).*(length(findstr(path,gswpath))==0))
@@ -7,4 +9,7 @@ function findgsw
  if((exist(gswlibpath,"dir")==7).*(length(findstr(path,gswlibpath))==0))
   addpath(gswlibpath);
  end%if
+ gsw_data = 'gsw_data_v3_0.mat';
+ gsw_data_file = which(gsw_data);
+ load(gsw_data_file,'version_number','version_date');
 end%function
