560eb6e8795836b2c56d09dab8301a9a9317d77f
Martin Hoecker-Martinez <mhoecker@coas.oregonstate,edu>
2013-10-16 12:05:40 -0700

    Add limiting depth and a window of times to average over
    average over a window of times arround wantdate,
    Set a maximum depth
    use findgsw to chech foe Gibbs Sea Water
    plot output as png in directory

--- LESinitialTS.m
+++ LESinitialTS.m
@@ -1,42 +1,38 @@
-function fileout = LESinitialTS(fileloc,filename,wantdate,outloc)
-#function fileout = LESinitialTS(fileloc,filename,date,outloc)
-# fileloc  - directiory of the data file
-# filename - name of the data file (no suffix, it is assumed to be .nc)
-# wantdate - date in matlab datenum format
-# outloc   - directory of output file (assume to be the currentdirectory if not given)
- if(nargin<4)
+function fileout = LESinitialTS(filename,wantdate,outloc,avgtime,maxdepth)
+ #function fileout = LESinitialTS(fileloc,filename,date,outloc)
+ # fileloc  - directiory of the data file
+ # filename - name of the data file (no suffix, it is assumed to be .nc)
+ # wantdate - date in matlab datenum format
+ # outloc   - directory of output file (assume to be the currentdirectory if not given)
+ if(nargin<3)
   outloc = "";
  end%if
- gswloc = "/home/mhoecker/work/TEOS-10/";
- gswlib = "/home/mhoecker/work/TEOS-10/library/";
- addpath(gswloc)
- addpath(gswlib)
- gsw_data = 'gsw_data_v3_0.mat';
- gsw_data_file = which(gsw_data)
- load(gsw_data_file,'version_number','version_date');
- ncfile = [fileloc filename ".nc"];
- nc = netcdf(ncfile,"r");
- t = nc{'th'}(:);
+ % Check for Gibbs Sea Water
+ [version_number,version_date] = findgsw;
+ nc = netcdf(filename,"r");
+ t = nc{'t'}(:);
  z = nc{'z'}(:);
- lat = nc{'lat'}(:);
- lon = nc{'lon'}(:);
- dateidx = find(abs(t-wantdate)==min(abs(t-wantdate)),1);
- dateused  = t(dateidx);
+ dateidx = find(abs(t-wantdate)<avgtime,1);
+ depthidx = find(abs(z)<=abs(maxdepth));
+ dateused  = mean(t(dateidx));
  clear t;
- Tc = nc{'Tc'}(dateidx,:);
- Sa = nc{'Sa'}(dateidx,:);
- P = nc{'P'}(:);
+ clear z;
+ z = nc{'z'}(depthidx);
+ Tc = nanmean(nc{'T'}(dateidx,depthidx),1);
+ Sa = nanmean(nc{'S'}(dateidx,depthidx),1);
+ P = gsw_p_from_z(z,0);
  idxgood = find(isnan(Tc).*isnan(Sa)==0);
  z = z(idxgood);
  Tc = Tc(idxgood);
  Sa = Sa(idxgood);
  P = P(idxgood);
- Sp = gsw_SP_from_SA(Sa,P,lon,lat);
+ Sp = gsw_SP_from_SA(Sa,P,80.5,0);
  ncclose(nc);
  #
- fileout = [outloc int2str(floor(dateused)) filename ".ic"];
+ outname = [outloc "TS_profiles" int2str(floor(dateused))]
+ fileout = [outname".ic"];
  outid = fopen(fileout,"w");
- fprintf(outid,'-z|Tc|Sp/%s.nc %sGMT lat %3.2f lon %3.2f\n',filename,datestr(dateused,"dd-mmm-yyyy HH"),lat,lon);
+ fprintf(outid,'-z|Tc|Sp/%s.nc %sGMT\n',filename,datestr(dateused,"dd-mmm-yyyy HH"));
  if(max(z)<0)
   imax = find(z==max(z));
   fprintf(outid,'%f %f %f\n',0.0,Tc(imax),Sp(imax))
@@ -45,4 +41,13 @@ function fileout = LESinitialTS(fileloc,filename,wantdate,outloc)
   fprintf(outid,'%f %f %f\n',abs(z(i)),Tc(i),Sp(i))
  end%for
  fclose(outid)
+ subplot(1,2,1)
+ plot(Tc,z)
+ xlabel("Temperature (C)")
+ ylabel("Depth (m)")
+ subplot(1,2,2)
+ plot(Sp,z)
+ xlabel("Salinity (psu)")
+ ylabel("Depth (m)")
+ print([outname ".png"],"-dpng")
 end%function
