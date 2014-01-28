7ad89571f98bb0a97f1a30b31f6a8ab635101f82
Martin Hoecker-Martinez <mhoecker@coas.oregonstate,edu>
2014-01-27 18:11:40 -0800

    try to make lomits file reusable with only gnuplot
    This may break a lot of things!

--- SkyllinstadEtAl1999/plotparam.m
+++ SkyllinstadEtAl1999/plotparam.m
@@ -1,16 +1,20 @@
-function [useoctplot,t0sim,dsim,tfsim] = plotparam(outdir,datdir)
+function [useoctplot,t0sim,dsim,tfsim,limitsfile,scriptdir] = plotparam(outdir,datdir,abrev)
  % Set the plot parameters for all the plots
+ if(nargin==1)
+  datdir = outdir;
+ end%if
+ if(nargin<3)
+  abrev = "";
+ end%if
  useoctplot=0; % 1 plot using octave syntax, 0 use gnuplot script
  t0sim = 328; % simulated start time is 2011 yearday 328
  dsim = 80; % Maximum simulation depth
  tfsim = t0sim+1.25; % Simulated stop time 2011 yearday
  palcolors = 8; % number of colors in the color bar palette
- if(nargin==1)
-  datdir = outdir;
- end%if
+ scriptdir = "/home/mhoecker/work/Dynamo/octavescripts/SkyllinstadEtAl1999/";
  if(useoctplot!=1)
   [gnuplotterm,termsfx] = termselect("pngposter");
-  limitsfile = [ "/home/mhoecker/tmp/limits.plt"];
+  limitsfile = [ outdir abrev "limits.plt"];
   fid = fopen(limitsfile,"w");
   fprintf(fid,"t0sim=%f\n",t0sim);
   fprintf(fid,"tfsim=%f\n",tfsim);
@@ -18,6 +22,7 @@ function [useoctplot,t0sim,dsim,tfsim] = plotparam(outdir,datdir)
   fprintf(fid,"palcolors=%f\n",palcolors);
   fprintf(fid,"outdir = '%s'\n",outdir);
   fprintf(fid,"datdir = '%s'\n",datdir);
+  fprintf(fid,"scriptdir = '%s'\n",scriptdir);
   fprintf(fid,"termsfx = '%s'\n",termsfx);
   fprintf(fid,"set term %s\n",gnuplotterm);
   fprintf(fid,"cbmarks = (palcolors+1)/2\n");
