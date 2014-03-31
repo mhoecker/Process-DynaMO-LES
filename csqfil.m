006e16341913d139c877019ee119d0baafe258f2
Martin Hoecker-Martinez <mhoecker@coas.oregonstate,edu>
2014-03-31 15:48:11 -0700

    sample filters for the first four orders

--- csqfil.m
+++ csqfil.m
@@ -1,7 +1,7 @@
 function y = csqfil(x,t,s,T)
  if(nargin==0)
   harmfill(0,0,0,0,4,"csqfill")
-  y = NaN
+  y = NaN;
  else
   y = harmfill(x,t,s,T,4)
  end%if
--- harmfill.m
+++ harmfill.m
@@ -17,7 +17,10 @@ function [y] = harmfill(x,t,s,T,order,demoname)
  % plot the filter frequency response
  % compare a moving average of same width
   N = 2^16;
-  harmfilldemo(N,order,"harmfill")
+  harmfilldemo(N,1,"harmfill1")
+  harmfilldemo(N,2,"harmfill2")
+  harmfilldemo(N,3,"harmfill3")
+  harmfilldemo(N,4,"harmfill4")
  elseif(nargin==6)
  % plot the filter frequency response
  % compare a moving average of same width
