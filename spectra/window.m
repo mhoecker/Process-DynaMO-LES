8dcaf1b3746b60880494a7b4d0f2711d4f88dbec
Martin Hoecker-Martinez <mhoecker@coas.oregonstate,edu>
2014-03-31 16:41:33 -0700

    Add option to remove variace boosting

--- spectra/window.m
+++ spectra/window.m
@@ -1,15 +1,20 @@
-function winy = window(y,order)
+function winy = window(y,order,NoBoost)
 % Returned a windowed version of the data using a sine window
 %  winy_n = y_n*sin((n-1/2)*pi/N).^order;
-
+% Set NoBoost=1 for no variace boosting
  if(nargin==1)
   order = 1;
  end%if
+ if(nargin<3)
+  NoBoost=0;
+ end%if
  N = length(y);
- ysq = y*y';
+ ysq = var(y,1);
  winy = linspace(1,N,N)-.5;
  winy = sin(winy*pi/N).^order;
  winy = winy.*y;
- wsq = winy*winy';
- winy = winy*sqrt(ysq/wsq);
+ wsq = var(winy,1);
+ if(NoBoost==0)
+  winy = winy*sqrt(ysq/wsq);
+ end%if
 end%function
