b85d7469623ae284935469147ce7a6290c96c16c
Martin Hoecker-Martinez <mhoecker@coas.oregonstate,edu>
2014-04-16 15:53:48 -0700

    add missing semi-colons, use gnuplot stats to set colorbar

--- plotspecVvar.m
+++ plotspecVvar.m
@@ -16,30 +16,31 @@ if nargin()<1
   tmp  = '/home/mhoecker/tmp/';
  end%if
  for i=1:length(roots)
-  datname = [datdir roots{i} '.dat']
-  outname = [plotdir roots{i} ]
+  datname = [datdir roots{i} '.dat'];
+  outname = [plotdir roots{i} ];
   logform = "'10^{%L}'";
   addpath /home/mhoecker/work/Dynamo/octavescripts
   [termtxt,termsfx] = termselect(term);
-  pltname = [tmp num2str(floor(now*3600*24*1000),"%011i") ".plt"];pause(.0006);
+  pltname = [tmp num2str(floor((now-floor(now))*24*60*60*1000),"%011i") ".plt"];pause(.0006);
   pltid = fopen(pltname,'w');
   fprintf(pltid,"set term %s\n",termtxt)
   fprintf(pltid,"set output '%s%s'\n",outname,termsfx)
   fprintf(pltid,"unset surface\n")
   fprintf(pltid,"set view map\n")
-  fprintf(pltid,"set logscale x\n")
-  fprintf(pltid,"set logscale cb\n")
-  fprintf(pltid,"set logscale z\n")
   fprintf(pltid,"set xlabel 'Horizontal Wavenumber (rad/m)'\n")
   fprintf(pltid,"set ylabel 'time (s)'\n")
-  fprintf(pltid,"set cblabel 'Spectral Energy Density (m^2/s^2/rad/m) '\n")
+  fprintf(pltid,"set cblabel 'Spectral Energy Density (m^2/s^2/rad/m) ' offset 2\n")
   fprintf(pltid,"unset key\n")
   fprintf(pltid,"set pm3d\n")
   fprintf(pltid,"set palette mode HSV\n")
-  fprintf(pltid,"%s",paltext("zissou"))
+  fprintf(pltid,"%s",paltext("zissou",9))
   fprintf(pltid,"set format x %s\n",logform)
   fprintf(pltid,"set format cb %s\n",logform)
-  fprintf(pltid,"set cbrange [1e0:*]\n")
+  fprintf(pltid,"stats '%s' binary matrix u 3\n",datname)
+  fprintf(pltid,"set logscale x\n")
+  fprintf(pltid,"set logscale cb\n")
+  fprintf(pltid,"set logscale z\n")
+  fprintf(pltid,"set cbrange [STATS_mean:STATS_max]\n")
   fprintf(pltid,"splot '%s' binary matrix\n",datname)
   fclose(pltid)
   unix(["gnuplot " pltname ])
