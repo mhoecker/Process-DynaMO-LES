7a17a6e333ced136b6b04f156fd4c486e0afc44c
Martin Hoecker-Martinez <mhoecker@coas.oregonstate,edu>
2014-04-22 12:03:42 -0700

    refine directional spread
    output a mask for which wave vectors are used (dtheta)
    use the approximation for
    k.mag = sqrt( k.paralell^2+k.perp^2 )
    k.mag ~ k.pralell*(1+.5(k.perp/k.paralell)^2)
    to set a limit on k.perpMAX
    k.mag < k.pralell*(1+.5(k.perpMAX/k.paralell)^2)

--- spectra/directionalSpec.m
+++ spectra/directionalSpec.m
@@ -1,16 +1,16 @@
-function [kr,Pr]=directionalSpec(theta,k,l,P)
+function [kr,Pr,dtheta]=directionalSpec(theta,k,l,P)
  dk = mean(diff(k));
  dl = mean(diff(l));
- dkr = sqrt((dk*cos(theta))^2+(dl*sin(theta))^2);
+ dkr = sqrt((dk^2+dl^2));
  rhat = [cos(theta),sin(theta)];
  kraw = [];
  Praw = [];
  [kk,ll] = meshgrid(k,l);
  kmag = sqrt(kk.^2+ll.^2);
- delta = 0.125*dkr/max(max(kmag));
  kdot = kk*rhat(1)+ll*rhat(2);
- dtheta = (1-kdot./kmag);
- goodidx = find(dtheta<delta);
+ dtheta = kdot.*(1+.5*(dkr./kdot).^2)-kmag;
+ dtheta = sign(dtheta);
+ goodidx = find(dtheta==1);
  Praw = P(goodidx);
  kraw = kdot(goodidx);
  [kraw,idx] = sort(kraw);
