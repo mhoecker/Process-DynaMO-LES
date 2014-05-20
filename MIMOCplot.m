cdedd950ad6e9a7a3489638988642ba3835ef678
Martin Hoecker-Martinez <mhoecker@coas.oregonstate,edu>
2014-05-20 16:51:14 -0700

    add some informative comments

--- MIMOCplot.m
+++ MIMOCplot.m
@@ -1,4 +1,8 @@
-function MIMOC
+function MIMOC()
 %
 % plot files downloded using
-%  wget http://www.pmel.noaa.gov/mimoc/data/MIMOC_Z_GRID_v2.2_CT_SA_month{01..12}.nc.gz
+%  wget http://www.pmel.noaa.gov/mimoc/data/MIMOC_ML_v2.2_CT_SA.zip
+% but the dimensions dont have variables associated with them
+% so rename them
+% ncrename -d LAT,LATITUDE -d LONG,LONGITUDE old.nc new.nc
+end%function
