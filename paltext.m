5b0a818ec96df196492ef4cfd848874d6826e1c1
Martin Hoecker-Martinez <mhoecker@coas.oregonstate,edu>
2014-08-08 12:11:28 -0700

    fix problems when increasing # of colors

--- paltext.m
+++ paltext.m
@@ -16,26 +16,24 @@ function palette = paltext(paltype,N,clims)
  if(nargin<2)
   N=9;
  end%if
+ n = num2str(N,"%i");
  crange = cmax-cmin;
+ cmid = num2str(-cmin/(crange));
  switch paltype
   case {"rainbow" "hue"}
-   palette = ["set palette mode HSV\nset palette functions (gray)*.9,.25+.75*(1-gray)**.5,.25+.75*gray**.5\n set palette maxcolors " num2str(N,"%i") "\n"];
+   palette = ["set palette mode HSV\nset palette functions (gray)*.9,.25+.75*(1-gray)**.5,.25+.75*gray**.5\n set palette maxcolors " n "\n"];
   case {"backrainbow" "euh"}
-   palette = ["set palette mode HSV\nset palette function (1-gray)*.9,.25+.75*(1-gray)**.5,.25+.75*gray**.5\n set palette maxcolors " num2str(N,"%i") "\n"];
+   palette = ["set palette mode HSV\nset palette function (1-gray)*.9,.25+.75*(1-gray)**.5,.25+.75*gray**.5\n set palette maxcolors " n "\n"];
   case { "plusminus" "pm" "minusplus" "mp" "bluered" "br" }
-   palette = ["set palette mode RGB\nset palette defined (0 'blue', " num2str(-.5*cmin/crange) " 'cyan', " num2str(-cmin/crange) " 'white', " num2str((.5*cmax-cmin)/crange) " 'yellow', 1 'red')\n set palette maxcolors " num2str(N,"%i") "\n"];
-  case { "pos" "positive" "red"}
-   palette = ["set palette mode RGB\nset palette defined (0 'white', .25 'yellow',  .75 'orange', 1 'red')\n set palette maxcolors " num2str(N,"%i") "\n set palette maxcolors " num2str(N,"%i") "\n"];
-  case { "neg" "negative" "blue"}
-   palette = ["set palette mode RGB\nset palette defined (0 'white', .3 'cyan', 1 'blue')\n set palette maxcolors " num2str(N,"%i") "\n"];
+   palette = ["set palette mode RGB\nxmid = " cmid ";r(x) = ( x>= xmid ? 1 : 0 );b(x) = ( x<=xmid ? 1 : 0 );g(x) = (x<xmid ? b(x)*((1-x)/(1-xmid))  : r(x)*(x/xmid));set palette function r(gray),g(gray),b(gray)\n set palette maxcolors " n "\n"];
+  case { "pos" "positive" "red" "posnan" "positivenan" "rednan"}
+   palette = ["set palette mode RGB\nset palette function 1,(1-gray),0\n set palette maxcolors " n "\n"];
+  case { "neg" "negative" "blue" "negnan" "negativenan" "bluenan"}
+   palette = ["set palette mode RGB\nset palette function 0,(1-gray),1\n set palette maxcolors " n "\n"];
   case { "plusminusnan" "pmnan" "minusplusnan" "mpnan" "bluerednan" "brnan" }
-   palette = ["set palette mode RGB\nset palette defined (0 'blue', " num2str(-.3*cmin/crange) " 'cyan', " num2str(-cmin/crange) " 'grey80', " num2str((.7*cmax-cmin)/crange) " 'yellow', 1 'red')\n set palette maxcolors " num2str(N,"%i") "\n"];
-  case { "posnan" "positivenan" "rednan"}
-   palette = ["set palette mode RGB\nset palette defined (0 'grey80', .25 'yellow', .75 'orange', 1 'red')\n set palette maxcolors " num2str(N,"%i") "\n"];
-  case { "negnan" "negativenan" "bluenan"}
-   palette = ["set palette mode RGB\nset palette defined (0 'grey80', .3 'cyan', 1 'blue')\n set palette maxcolors " num2str(N,"%i") "\n"];
+   palette = ["set palette mode RGB\nxmid = " cmid ";dx = .1/" n ";r(x) = 1-exp(-1-(x-xmid)/dx);b(x) = 1-exp(-1+(x-xmid)/dx);g(x) = (x>xmid ? r(x)*((1-x)/(1-xmid))  : b(x)*(x/xmid));set palette function r(gray),g(gray),b(gray)\n set palette maxcolors " n "\n"];
   case { "zissou" }
-   palette = ["set palette mode RGB\nset palette defined (0 '#3B9AB2', .25 '#78B7C5', .5 '#EBCC2A', .75 '#E1AF00', 1 '#F21A00')\n set palette maxcolors " num2str(N,"%i") "\n"];
+   palette = ["set palette mode RGB\nset palette defined (0 '#3B9AB2', .25 '#78B7C5', .5 '#EBCC2A', .75 '#E1AF00', 1 '#F21A00')\n set palette maxcolors " n "\n"];
   case { "zissoublocks" }
    palette = ["set palette mode RGB\nset palette defined (0 '#3B9AB2',.2 '#3B9AB2', .2 '#78B7C5', .4 '#78B7C5', .4 '#EBCC2A', .6 '#EBCC2A', .6 '#E1AF00', .8 '#E1AF00', .8 '#F21A00', 1 '#F21A00')\n set palette maxcolors 5\n"];
   otherwise
