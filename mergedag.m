e671ad27e77b935c793723225ef1aa85e471ca13
Martin Hoecker-Martinez <mhoecker@coas.oregonstate,edu>
2014-09-02 14:57:53 -0700

    make a functional dag sticher
    Assumes the files are good to the final time point

--- mergedag.m
+++ mergedag.m
@@ -1,9 +1,42 @@
-function mergedag(daglist,dagout)
- dag = netcdf(daglist{1},'r');
+function [t,idxs] = mergedag(daglist,dagdir)
+ if(nargin()<1)
+  daglist = {'a','b','c','d','e','f','g','h'};
+ end%if
+ if(nargin()<2)
+  dagdir = '/home/mhoecker/work/Dynamo/output/yellowstone8/dyn1024-';
+ end%if
+ dagfile = [dagdir daglist{1} '_dag.nc'];
+ dag = netcdf(dagfile,'r');
  t = squeeze(dag{'time'}(:));
- idxs = [1,length(t)];
+ idxs = [1,length(t)-1];
+ ncclose(dag);
+ cliplist = {dagfile};
  for i=2:length(daglist)
-  dag = netcdf(daglist{i},'r');
-  t1 = squeeze(dag{'time'}(:));
-  idxs = [idxs;find(t1>max(t),1),lengtht1];
+  dagfile = [dagdir daglist{i} '_dag.nc'];
+  dag = netcdf(dagfile,'r');
+  ti = squeeze(dag{'time'}(:));
+  ncclose(dag);
+  idxi= [find(ti>max(t),1),length(ti)-1];
+  clipfile =  [dagdir daglist{i} '_dagclip.nc'];
+  ncoclip(dagfile,'time',idxi,clipfile)
+  cliplist = {cliplist{:},clipfile};
+  t = [t;ti(idxi(1):end)];
+  idxs = [idxs;idxi];
  end%for
+ outfile = [dagdir 'dag.nc'];
+ ncorcat(cliplist,outfile);
+end%function
+
+function ncoclip(infile,dimname,index,outfile)
+ ncksargs = [' -O -d ' dimname ',' num2str(index(1),"%i") ',' num2str(index(2),"%i") ' ' infile ' ' outfile];
+ unix( ['ncks' ncksargs])
+end%function
+
+function ncorcat(infiles,outfile)
+  files = '';
+ for(i=1:length(infiles))
+  files = [files infiles{i} ' '];
+ end%for
+ ncrcatargs = ['-O ' files ' ' outfile];
+ unix( ['ncrcat ' ncrcatargs])
+end%function
