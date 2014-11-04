fd1ae1ba1f1c04e9f69d81f683bd768184420fea
Martin Hoecker-Martinez <mhoecker@coas.oregonstate,edu>
2014-11-04 13:17:08 -0800

    use ddzinterp set default three point stencil

--- ddz.m
+++ ddz.m
@@ -1,7 +1,10 @@
 function [d,dsq]=ddz(z,order)
-% derivative matricies for independent variable z.
-% Three point stencil.
-% One-sided derivatives at boundaries.
-% no assumptions about z spacing.
-[val,d,dsq]=ddzinterp(z,z,order)
+ % derivative matricies for independent variable z.
+ % Three point stencil.
+ % One-sided derivatives at boundaries.
+ % no assumptions about z spacing.
+ if(nargin<2)
+  order=3;
+ end%if
+ [val,d,dsq]=ddzinterp(z,z,3);
 end
