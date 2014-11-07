65c4acdbc142889b06ade04a9815e58a5365a80a
Martin Hoecker-Martinez <mhoecker@coas.oregonstate,edu>
2014-11-07 10:17:25 -0800

    add some comments for usability

--- ddz.m
+++ ddz.m
@@ -3,8 +3,14 @@ function [d,dsq]=ddz(z,order)
  % Three point stencil.
  % One-sided derivatives at boundaries.
  % no assumptions about z spacing.
+ % example
+ % z = 1:10
+ % y = z.*z
+ % [der,dsq] = ddz(z,3)
+ % dydz = der*y'
+ % dsqydz = dsq*y'
  if(nargin<2)
   order=3;
  end%if
- [val,d,dsq]=ddzinterp(z,z,3);
+ [val,d,dsq]=ddzinterp(z,z,order);
 end
--- ddzinterp.m
+++ ddzinterp.m
@@ -1,5 +1,16 @@
 function [val,der,dsq] = ddzinterp(z,Z,order)
- # A matrix to calculate derrivatives on a new basis set
+ % A matrix to calculate derrivatives on a new basis set
+ % z is the points where the values are given
+ % Z are the point where you want the value and derrivatives
+ %
+ % example
+ % z = 1:10
+ % Z = .5:1:9.5
+ % y = z.*z
+ % [val,der,dsq] = ddzinterp(z,Z,order)
+ % Y = val*y'
+ % dYdZ = der*y'
+ % dsqYdZ = dsq*y'
  if(nargin==1)
   Z=z;
  end%if
@@ -23,13 +34,13 @@ function [val,der,dsq] = ddzinterp(z,Z,order)
    for j=1:order
     for k=1:order
      A(j,k) = (1/factorial(k-1))*diffz(idx(j),i).^(k-1);
-    end#for
-   end#for
+    end%for
+   end%for
    ainv = inv(A');
    for j=1:order
     val(i,idx(j))  = ainv(j,1);
     der(i,idx(j)) = ainv(j,2);
     dsq(i,idx(j)) = ainv(j,3);
-   end#for
-  end#for
-end#function
+   end%for
+  end%for
+end%function
