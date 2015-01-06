c6874cfb20a6fbc795a14445113651160107a80d
Martin Hoecker-Martinez <mhoecker@coas.oregonstate,edu>
2015-01-05 18:14:14 -0800

    use the value closest to the surface

--- getMLD.m
+++ getMLD.m
@@ -1,12 +1,11 @@
-function [MLD,MLI,drho,rho,DRHO]=getMLD(S,T,Z,DRHO)
+function [MLD,MLI,drho,rho,DRHO]=getMLD(S,T,Z,DRHO,min)
  if(nargin<4)
   DRHO=0.01;
  end%if
  vardims = size(T);
  zdim = find(vardims==length(Z));
  tdim = find(vardims!=length(Z));
- ZZ = ones(vardims).*Z';
- findgsw;
+ ZZ = ones(vardims).*abs(Z)';
  findgsw;
  g = gsw_grav(0);
  % Calculate pressure
@@ -18,12 +17,36 @@ function [MLD,MLI,drho,rho,DRHO]=getMLD(S,T,Z,DRHO)
  % Calculate density(S,T,Z)
  rho = gsw_rho(SA,CT,P*0);
  %rhobar = nanmean(nanmean(rho,2))
- drho = (rho.-min(rho,[],zdim));
+ nanidx = find(isnan(rho));
+ ZZ(nanidx) = max(abs(Z));
+ drho = rho;
+ Zsfc = zeros(vardims(tdim),1);
+ for i=1:vardims(tdim)
+  if(zdim==1)
+   rhoi=rho(:,i);
+   nanidx= find(isnan(rhoi));
+   Zgood = abs(Z);
+   Zgood(nanidx) = max(Zgood);
+   izsfc = find(Zgood==min(Zgood),1);
+   Zsfc(i) = Zgood(izsfc);
+   drho(:,i) = (rho(:,i).-rho(izsfc,i));
+  else
+   rhoi=rho(i,:);
+   nanidx= find(isnan(rhoi));
+   Zgood = abs(Z);
+   Zgood(nanidx) = max(Zgood);
+   izsfc = find(Zgood==min(Zgood));
+   Zsfc(i) = Zgood(izsfc);
+   drho(i,:) = (rho(i,:).-rho(i,izsfc));
+  end%if
+ end%for
  badidx = find(drho>DRHO);
+ nanidx = find(isnan(drho));
  ZZ(badidx) = 0;
- MLD = min(ZZ,[],zdim);
+ ZZ(nanidx) = max(abs(Z));
+ MLD = min(-ZZ,[],zdim);
  MLI=MLD;
  for i=1:length(MLD)
-  MLI(i) = find(Z==MLD(i),1);
+  MLI(i) = find(abs(Z)==-MLD(i),1);
  end%for
 end%
