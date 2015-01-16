ba51a19cdb4299d37cb19e9ed49b6e09789c1857
Martin Hoecker-Martinez <mhoecker@coas.oregonstate,edu>
2015-01-16 14:51:45 -0800

    use as a call to changevar2

--- isobathyize.m
+++ isobathyize.m
@@ -1,3 +1,14 @@
-function isoB = isobathyize(t,rho,U,V,SA,CT,z_cords)
+function isoB = isobathyize(t,rho,z,U,V,SA,CT,z_cords)
+
+
+ fields = {t,rho,U,V,CT,SA};
+ changed = changevar2(t,z,fields,z_cords);
+ isoB.t    = changed.x;
+ isoB.z    = changed.z
+ isoB.rho  = changed.fields{1};
+ isoB.U    = changed.fields{2};
+ isoB.V    = changed.fields{3};
+ isoB.CT   = changed.fields{4};
+ isoB.SA   = changed.fields{5};
 
 end%function
