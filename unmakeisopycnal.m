461aa99f52f531e606be8454714739ef113bd728
Martin Hoecker-Martinez <mhoecker@coas.oregonstate,edu>
2015-01-21 08:52:34 -0800

    use the multiple average times

--- unmakeisopycnal.m
+++ unmakeisopycnal.m
@@ -25,19 +25,27 @@ z = floor(min(min(fields{1}))):ceil(max(max(fields{1})));
 %plot variables for debuging
 if(makeplots!=0)
  [zz,tt] = meshgrid(z,t);
- size(zz)
- size(tt)
- size(changed.fields{1})
- size(changed.fields{2})
  titles = {"rho","U","V","CT","SA"};
+ medfil = 13;
  for i=1:length(changed.fields);
+  for j=1:length(z)
+   jmin = max([1,ceil( j-medfil/2)]);
+   jmax = min([length(z),floor(j+medfil/2)]);
+   for k=1:length(t)
+    varin = changed.fields{i}(k,jmin:jmax);
+    idxgood = find(~isnan(varin));
+    vargood = varin(idxgood);
+    changed.fields{i}(k,j)=median(vargood);
+   end%for
+  end%for
   figfile = ["/home/mhoecker/tmp/unmakeisop" num2str(i,"%03i") ".png"];
   figure(i)
   subplot(1,1,1)
-  plot(changed.fields{i},z); shading flat
+  pcolor(tt,zz,changed.fields{i}); shading flat; colorbar
   title(titles{i})
   print(figfile,"-dpng")
  end%for
 end%if
 %save new netcdf with varibles of z,t
+
 
