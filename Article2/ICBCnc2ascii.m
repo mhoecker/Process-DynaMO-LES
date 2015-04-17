4ab6d18cdd953427fb08d1a10db7a3e8a5590ab1
Martin Hoecker-Martinez <mhoecker@coas.oregonstate,edu>
2015-04-16 17:20:28 -0700

    add missing parens plot velocity profiles

--- Article2/ICBCnc2ascii.m
+++ Article2/ICBCnc2ascii.m
@@ -14,27 +14,46 @@ function [flxout,TSout,UVout] = ICBCnc2ascii(ICBCfile,outloc)
   outloc = "/home/mhoecker/tmp/";
   #outloc = "/media/mhoecker/8982053a-3b0f-494e-84a1-98cdce5e67d9/Dynamo/output/nextrun/";
  end%if
- flxout = LESsurfBC(ICBCfile,outloc);
+ BCnc2ascii(ICBCfile,outloc);
  TSout = LESinitialTS(ICBCfile,outloc);
+ unix(["cp " ICBCfile " " outloc "UVinit.nc"]);
+ nc = netcdf([outloc "UVinit.nc"],'r');
+ subplot(1,2,1)
+ plot(nc{"U"}(:),nc{"Z"}(:))
+ xlabel("U (m/s)")
+ ylabel("Depth (m)")
+ subplot(1,2,2)
+ plot(nc{"V"}(:),nc{"Z"}(:))
+ xlabel("V (m/s)")
+ ylabel("Depth (m)")
+ print([outloc "UV.png"],"-dpng")
+ ncclose(nc);
 end%function
 
 function BCnc2ascii(ICBCfile,outloc)
- outname = [outloc "Surface_Flux_" int2str(floor(min(t))) "-" int2str(ceil(max(t)))];
+ outname = [outloc "Surface_Flux"];
  fileout = [outname ".bc"];
+ nc = netcdf(ICBCfile,'r');
  outid = fopen(fileout,"w");
- fprintf(outid,'SourceFile=%s\n',ICBCfile);
- fprintf(outid,' \n');
+ fprintf(outid,'Source File %s\n',ICBCfile);
+ fprintf(outid,'Date %s \n',date);
  fprintf(outid,' swf_top, hf_top, lhf_top, rain, ustr_t, vstr_t, wave_l, wave_h, w_angle\n');
  for i=1:length(nc{"t"}(:));
   fprintf(outid,'%f ',nc{"t"}(i)/3600);
-  fprintf(outid,'%f ',nc{"J_sw"}(i);
-  fprintf(outid,'%f ',nc{"J_se"}(i);
-  fprintf(outid,'%f ',nc{"J_la"}(i);
-  fprintf(outid,'%f ',nc{"P"}(i);
-  fprintf(outid,'%f ',nc{"tau_x"}(i);
-  fprintf(outid,'%f ',nc{"tau_y"}(i);
-  fprintf(outid,'%f ',nc{"W_l"}(i);
-  fprintf(outid,'%f ',nc{"W_h"}(i);
-  fprintf(outid,'%f ',nc{"W_d"}(i);
+  fprintf(outid,'%f ',nc{"J_sw"}(i));
+  fprintf(outid,'%f ',nc{"J_lw"}(i));
+  fprintf(outid,'%f ',nc{"J_la"}(i));
+  fprintf(outid,'%f ',nc{"P"}(i));
+  fprintf(outid,'%f ',nc{"tau_x"}(i));
+  fprintf(outid,'%f ',nc{"tau_y"}(i));
+  fprintf(outid,'%f ',nc{"W_l"}(i));
+  fprintf(outid,'%f ',nc{"W_h"}(i));
+  fprintf(outid,'%f ',nc{"W_d"}(i));
   fprintf(outid,'\n');
  end%for
+ fclose(outid);
+ [stat,wdir] = unix('pwd');
+ unix(["cd " outloc]);
+ unix(['gnuplot -e "file=' "'" outname "'" '"  /home/mhoecker/work/Dynamo/octavescripts/plotLESbc.plt']);
+ unix(["cd " wdir]);
+end%function
