ab380db91e6deeac07c0cbcca52fc38845a968d0
Martin Hoecker-Martinez <mhoecker@coas.oregonstate,edu>
2015-07-03 23:44:57 -0700

    plot the full depth for an hour

--- LESsummaries/plotparam.m
+++ LESsummaries/plotparam.m
@@ -16,8 +16,8 @@ function [useoctplot,t0sim,dsim,tfsim,limitsfile,dir] = plotparam(outdir,abrev)
  useoctplot=0; % 1 plot using octave syntax, 0 use gnuplot script
  t0sim = 0; % simulated start time is 0
  dsim = 128; % Maximum simulation depth
- dplt = dsim*.8; % Maximum depth of plots
- tfsim = t0sim+12./(24); % Simulated stop time 2011 yearday
+ dplt = dsim*1.0; % Maximum depth of plots
+ tfsim = t0sim+1./(24); % Simulated stop time 2011 yearday
  palcolors = 11; % number of colors in the color bar palette
  trange = [t0sim,tfsim];
  if((useoctplot==0))
